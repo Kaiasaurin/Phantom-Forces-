fab1a3b7d31d6aa3ca7fba9207cfc0496aae3dd1
Kaiasaurin <101678546+Kaiasaurin@users.noreply.github.com>
2022-04-12 17:04:04 -0700

    Add files via upload

---
+++ PF Hitbox Torso.lua
@@ -0,0 +1,11 @@
+local Size=10
+local Target="Torso"
+game.RunService.RenderStepped:Connect(function()
+    for _,a in pairs(game.Workspace.Players:GetChildren())do
+        if a.Name~=game.Players.LocalPlayer.Team.Name then
+            for _,b in pairs(a:GetChildren())do
+                b[Target].Size=Vector3.new(Size,Size,Size)
+            end
+        end
+    end
+end)
